40f755a3af042374720b8a285b3a45524f933761
ailenpaez <a.jorgelinapaez@gmail.com>
2025-03-17 04:40:12 -0300

    Permisos Google y credenciales.

--- src/google_calendar.py
+++ src/google_calendar.py
@@ -6,3 +6,15 @@ from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
+
+#Permisos accesos al calendar.
+
+SCOPES = ['https://www.googleapis.com/auth/calendar.readonly',]
+
+def authenticate():
+     """Auth to Google Calendar API."""
+     creds= None
+     
+     if os.path.exist('token.json'): #buscar token
+          creds = Credentials.from_authorized_user_info(
+            json.loads(open('token.json', 'r').read()), SCOPES)
